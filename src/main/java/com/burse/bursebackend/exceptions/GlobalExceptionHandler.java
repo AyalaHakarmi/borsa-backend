7e07ebed9c60fb16f81a8717bcf54503c2a3e775
Ayala Hakarmi <Ayalahakarmi@gmail.com>
2025-08-04 12:37:46 +0300

    Exception handler improvement

--- src/main/java/com/burse/bursebackend/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/burse/bursebackend/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.burse.bursebackend.exceptions;
 
+import com.burse.bursebackend.types.ErrorCode;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,7 +15,20 @@ public class GlobalExceptionHandler {
     public ResponseEntity<?> handleBurseException(BurseException ex) {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body(Map.of("error", ex.getMessage()));
+                .body(Map.of(
+                        "code", ex.getErrorCode().name(),
+                        "error", ex.getMessage()
+                ));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGeneralException(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(Map.of(
+                        "code", ErrorCode.UNKNOWN,
+                        "error", ex.getMessage()
+                ));
     }
 }
 
